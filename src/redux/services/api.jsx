e608bd922ff18fe9977c138544289655fc7632b4
vishal2024-Shilsha <vishal.singh@shilshatech.com>
2026-04-06 10:33:24 +0530

    added animation

--- src/redux/services/api.jsx
+++ src/redux/services/api.jsx
@@ -6,14 +6,13 @@ export const base = import.meta.env.VITE_BASE_URL;
 const baseQueryWith401Handling = async (args, api, extraOptions) => {
   const result = await rawBaseQuery(args, api, extraOptions);
   let localValue=null
-  if ((result?.error?.status === 401 || result?.error?.status ===403) && (window.location.pathname !== "/" && window.location.pathname !=='/admin-login' && window.location.pathname !=='/employee/login')) {
+  if ((result?.error?.status === 401 || result?.error?.status ===403) && (window.location.pathname !== "/login" && window.location.pathname !=='/admin-login' && window.location.pathname !=='/employee/login')) {
     // ✅ Clear storage
     if(localStorage.getItem('role')=="sub_vendor"){
       localValue="sub_vendor"
     }
     localStorage.clear();
     sessionStorage.clear();
-
     // ✅ Redirect to login
     if(localValue=="sub_vendor"){
     window.location.replace("/employee/login");
